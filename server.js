b4c2c952e4e43c3228326439d065aecc0b71b555
cuellar2020 <67763251+cuellar2020@users.noreply.github.com>
2022-05-06 13:52:39 -0400

    cambiaso

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyparser.urlencoded({
 }));
 app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
-app.set('view engine', 'hbs');
+app.engine('html', exphbs({ extname: 'html', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'html');
 
 app.listen(3000, () => {
     console.log('Server : 3000');
