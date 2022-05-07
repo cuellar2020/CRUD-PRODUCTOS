88bb08a0cf1477d2f4f9ca2631a1aab91322a52b
cuellar2020 <67763251+cuellar2020@users.noreply.github.com>
2022-05-06 23:17:21 -0400

    reload2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser');
-
+const port = process.env.PORT || 8080;
 const usuarioController = require('./controllers/usuarioCotroller');
 
 var app = express();
@@ -16,8 +16,8 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('html', exphbs({ extname: 'html', defaultLayout: 'index', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'html');
 
-app.listen(3000, () => {
-    console.log('Server : 3000');
-});
+app.listen(port, function () {
+    console.log(`Example app listening on ${port}!`);
+  });
 
 app.use('/producto', usuarioController);
